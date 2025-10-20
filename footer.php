96f913992edf34aae0d094faba7d5b1cdf850544
glmynixx <158756545+glmynixx@users.noreply.github.com>
2025-10-20 15:13:54 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+    <!-- Footer -->
+    <footer class="footer-container">
+        <div class="footer-bottom">
+            <p>© 2025 Aimazon. All rights reserved.</p>
+        </div>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
